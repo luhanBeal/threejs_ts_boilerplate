63bc31a9e913b3e25105fbdf7bdbe3efdcc4fc42
Luhan Beal <luhansantanna@gmail.com>
2024-06-09 14:31:13 +0100

    GUI using transformation matrix to move the cube

--- src/main.ts
+++ src/main.ts
@@ -8,99 +8,151 @@ const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(4, 4, 4)
+camera.position.set(-2, 4, 5)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.target.set(8, 0, 0)
-controls.update()
+new OrbitControls(camera, renderer.domElement)
 
-const light = new THREE.PointLight(0xffffff, 400)
-light.position.set(10, 10, 10)
-scene.add(light)
+const boxGeometry = new THREE.BoxGeometry()
+//const sphereGeometry = new THREE.SphereGeometry()
+//const icosahedronGeometry = new THREE.IcosahedronGeometry()
 
-const object1 = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshPhongMaterial({ color: 0xff0000 }))
-object1.position.set(4, 0, 0)
-scene.add(object1)
-object1.add(new THREE.AxesHelper(5))
+console.log(boxGeometry)
 
-const object2 = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshPhongMaterial({ color: 0x00ff00 }))
-object2.position.set(4, 0, 0)
-object1.add(object2)
-object2.add(new THREE.AxesHelper(5))
-
-const object3 = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshPhongMaterial({ color: 0x0000ff }))
-object3.position.set(4, 0, 0)
-object2.add(object3)
-object3.add(new THREE.AxesHelper(5))
-
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
+const material = new THREE.MeshNormalMaterial({
+  wireframe: true,
 })
 
-const gui = new GUI()
-const object1Folder = gui.addFolder('Object1 (Red Ball)')
-object1Folder.add(object1.position, 'x', 0, 10, 0.01).name('X Position')
-object1Folder.add(object1.rotation, 'x', 0, Math.PI * 2, 0.01).name('X Rotation')
-object1Folder.add(object1.scale, 'x', 0, 2, 0.01).name('X Scale')
-object1Folder.open()
-const object2Folder = gui.addFolder('Object2 (Green Ball)')
-object2Folder.add(object2.position, 'x', 0, 10, 0.01).name('X Position')
-object2Folder.add(object2.rotation, 'x', 0, Math.PI * 2, 0.01).name('X Rotation')
-object2Folder.add(object2.scale, 'x', 0, 2, 0.01).name('X Scale')
-object2Folder.open()
-const object3Folder = gui.addFolder('Object3 (Blue Ball)')
-object3Folder.add(object3.position, 'x', 0, 10, 0.01).name('X Position')
-object3Folder.add(object3.rotation, 'x', 0, Math.PI * 2, 0.01).name('X Rotation')
-object3Folder.add(object3.scale, 'x', 0, 2, 0.01).name('X Scale')
-object3Folder.open()
+const cube = new THREE.Mesh(boxGeometry, material)
+//cube.position.x = -4
+scene.add(cube)
+
+// const sphere = new THREE.Mesh(sphereGeometry, material)
+// sphere.position.x = 0
+// scene.add(sphere)
+
+// const icosahedron = new THREE.Mesh(icosahedronGeometry, material)
+// scene.add(icosahedron)
+// sphere.position.x = 4
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
+const gui = new GUI()
+
+// const cubeData = {
+//   width: 1,
+//   height: 1,
+//   depth: 1,
+//   widthSegments: 1,
+//   heightSegments: 1,
+//   depthSegments: 1
+// }
+
+const cubeFolder = gui.addFolder('Cube')
+const cubeRotationFolder = cubeFolder.addFolder('Rotation')
+cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2, 0.01)
+cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2, 0.01)
+cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2, 0.01)
+const cubePositionFolder = cubeFolder.addFolder('Position')
+cubePositionFolder.add(cube.position, 'x', -10, 10)
+cubePositionFolder.add(cube.position, 'y', -10, 10)
+cubePositionFolder.add(cube.position, 'z', -10, 10)
+const cubeScaleFolder = cubeFolder.addFolder('Scale')
+cubeScaleFolder.add(cube.scale, 'x', -5, 5, 0.1) // .onFinishChange(function() { console.log(cube.geometry) })
+cubeScaleFolder.add(cube.scale, 'y', -5, 5, 0.1)
+cubeScaleFolder.add(cube.scale, 'z', -5, 5, 0.1)
+// cubeFolder
+//   .add(cubeData, 'width', 1, 30)
+//   .onChange(regenerateBoxGeometry)
+//   .onFinishChange(function() { console.log(cube.geometry) })
+// cubeFolder.add(cubeData, 'height', 1, 30).onChange(regenerateBoxGeometry)
+// cubeFolder.add(cubeData, 'depth', 1, 30).onChange(regenerateBoxGeometry)
+// cubeFolder.add(cubeData, 'widthSegments', 1, 30).onChange(regenerateBoxGeometry)
+// cubeFolder.add(cubeData, 'heightSegments', 1, 30).onChange(regenerateBoxGeometry)
+// cubeFolder.add(cubeData, 'depthSegments', 1, 30).onChange(regenerateBoxGeometry)
+cubeFolder.open()
+
+// function regenerateBoxGeometry() {
+//   const newGeometry = new THREE.BoxGeometry(
+//     cubeData.width,
+//     cubeData.height,
+//     cubeData.depth,
+//     cubeData.widthSegments,
+//     cubeData.heightSegments,
+//     cubeData.depthSegments
+//   )
+//   cube.geometry.dispose()
+//   cube.geometry = newGeometry
+// }
+
+// const sphereData = {
+//   radius: 1,
+//   widthSegments: 8,
+//   heightSegments: 6,
+//   phiStart: 0,
+//   phiLength: Math.PI * 2,
+//   thetaStart: 0,
+//   thetaLength: Math.PI
+// }
+// const sphereFolder = gui.addFolder('Sphere')
+// sphereFolder.add(sphereData, 'radius', 0.1, 30).onChange(regenerateSphereGeometry)
+// sphereFolder.add(sphereData, 'widthSegments', 1, 32).onChange(regenerateSphereGeometry)
+// sphereFolder.add(sphereData, 'heightSegments', 1, 16).onChange(regenerateSphereGeometry)
+// sphereFolder.add(sphereData, 'phiStart', 0, Math.PI * 2).onChange(regenerateSphereGeometry)
+// sphereFolder.add(sphereData, 'phiLength', 0, Math.PI * 2).onChange(regenerateSphereGeometry)
+// sphereFolder.add(sphereData, 'thetaStart', 0, Math.PI).onChange(regenerateSphereGeometry)
+// sphereFolder.add(sphereData, 'thetaLength', 0, Math.PI).onChange(regenerateSphereGeometry)
+// sphereFolder.open()
+
+// function regenerateSphereGeometry() {
+//   const newGeometry = new THREE.SphereGeometry(
+//     sphereData.radius,
+//     sphereData.widthSegments,
+//     sphereData.heightSegments,
+//     sphereData.phiStart,
+//     sphereData.phiLength,
+//     sphereData.thetaStart,
+//     sphereData.thetaLength
+//   )
+//   sphere.geometry.dispose()
+//   sphere.geometry = newGeometry
+// }
+
+// const icosahedronData = {
+//   radius: 1,
+//   detail: 0
+// }
+// const icosahedronFolder = gui.addFolder('Icosahedron')
+// icosahedronFolder.add(icosahedronData, 'radius', 0.1, 10).onChange(regenerateIcosahedronGeometry)
+// icosahedronFolder.add(icosahedronData, 'detail', 0, 5).step(1).onChange(regenerateIcosahedronGeometry)
+// icosahedronFolder.open()
+
+// function regenerateIcosahedronGeometry() {
+//   const newGeometry = new THREE.IcosahedronGeometry(icosahedronData.radius, icosahedronData.detail)
+//   icosahedron.geometry.dispose()
+//   icosahedron.geometry = newGeometry
+// }
+
 const debug = document.getElementById('debug') as HTMLDivElement
 
 function animate() {
-    requestAnimationFrame(animate)
+  requestAnimationFrame(animate)
 
-    renderer.render(scene, camera)
+  renderer.render(scene, camera)
 
-    const object1WorldPosition = new THREE.Vector3()
-    object1.getWorldPosition(object1WorldPosition)
-    const object2WorldPosition = new THREE.Vector3()
-    object2.getWorldPosition(object2WorldPosition)
-    const object3WorldPosition = new THREE.Vector3()
-    object3.getWorldPosition(object3WorldPosition)
+  debug.innerText = 'Matrix\n' + cube.matrix.elements.toString().replace(/,/g, '\n')
 
-    debug.innerText =
-        'Red\n' +
-        'Local Pos X : ' +
-        object1.position.x.toFixed(2) +
-        '\n' +
-        'World Pos X : ' +
-        object1WorldPosition.x.toFixed(2) +
-        '\n' +
-        '\nGreen\n' +
-        'Local Pos X : ' +
-        object2.position.x.toFixed(2) +
-        '\n' +
-        'World Pos X : ' +
-        object2WorldPosition.x.toFixed(2) +
-        '\n' +
-        '\nBlue\n' +
-        'Local Pos X : ' +
-        object3.position.x.toFixed(2) +
-        '\n' +
-        'World Pos X : ' +
-        object3WorldPosition.x.toFixed(2) +
-        '\n'
-
-    stats.update()
+  stats.update()
 }
 
 animate()
