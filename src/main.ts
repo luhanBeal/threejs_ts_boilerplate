35626141bf8c58f89339dbb06efc50668d32b682
Luhan Beal <luhansantanna@gmail.com>
2024-06-09 12:32:45 +0100

    delta clock render added for animations time controll

--- src/main.ts
+++ src/main.ts
@@ -1,77 +1,48 @@
-import './style.css';
-import * as THREE from 'three';
-import Stats from 'three/addons/libs/stats.module.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GUI } from 'dat.gui';
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
 
 const scene = new THREE.Scene()
-scene.add(new THREE.GridHelper())
 
-const camera = new THREE.OrthographicCamera(-4, 4, 4, -4, -5, 10)
-camera.position.set(1, 1, 1)
-camera.lookAt(0, 0.5, 0)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 1.5
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 window.addEventListener('resize', () => {
-  //camera.aspect = window.innerWidth / window.innerHeight
+  camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
-});
+})
 
-new OrbitControls(camera, renderer.domElement);
+new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-cube.position.y = 0.5
 scene.add(cube)
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-const gui = new GUI()
-
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'x', -10, 10)
-cameraFolder.add(camera.position, 'y', -10, 10)
-cameraFolder.add(camera.position, 'z', -10, 10)
-cameraFolder.add(camera, 'left', -10, 0).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'right', 0, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'top', 0, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'bottom', -10, 0).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'near', -5, 5).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'far', 0, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.open()
+const clock = new THREE.Clock();
+let delta;
 
 function animate() {
   requestAnimationFrame(animate)
 
-  camera.lookAt(0, 0.5, 0)
+  delta = clock.getDelta();
 
-  stats.begin();
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
-  stats.end();
+  cube.rotation.x += 0.1 * delta;
+  cube.rotation.y += 0.1 * delta;
 
   renderer.render(scene, camera)
 
-  // stats.update();
+  stats.update()
 }
 
 animate()
