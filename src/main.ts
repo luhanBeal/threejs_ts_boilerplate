68f422e80b90fe873266449b72349f9c88df5e41
Luhan Beal <luhansantanna@gmail.com>
2024-06-15 19:23:26 +0100

    shadow and light on mesh inported gltf objects

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,34 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 import Stats from 'three/addons/libs/stats.module.js'
+import { Lensflare, LensflareElement } from 'three/addons/objects/Lensflare.js'
 
 const scene = new THREE.Scene()
 
-new RGBELoader().load('img/venice_sunset_1k.hdr', (texture) => {
+const light = new THREE.SpotLight(undefined, Math.PI * 1000)
+light.position.set(5, 5, 5)
+light.angle = Math.PI / 16
+light.castShadow = true
+scene.add(light)
+
+// const helper = new THREE.SpotLightHelper(light)
+// scene.add(helper)
+
+new RGBELoader().load('img/symmetrical_garden_02_1k.hdr', (texture) => {
   texture.mapping = THREE.EquirectangularReflectionMapping
   scene.environment = texture
   scene.background = texture
-  scene.backgroundBlurriness = 1.0
 })
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.set(2, 1, -2)
+camera.position.set(1.5, 0.75, 2)
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
+// Reduce the intensity of the light(exposure) on the enviroment
 renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 0.1
+// -----
+renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -29,68 +42,28 @@ window.addEventListener('resize', () => {
 })
 
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.target.y = 0.75
 controls.enableDamping = true
 
-// const loader = new GLTFLoader()
-// loader.load('models/suv_body.glb', (gltf) => {
-//   scene.add(gltf.scene)
-// })
-// loader.load('models/suv_wheel.glb', (gltf) => {
-//   gltf.scene.position.set(-0.65, 0.2, -0.77)
-//   scene.add(gltf.scene)
-// })
-// loader.load('models/suv_wheel.glb', (gltf) => {
-//   gltf.scene.position.set(0.65, 0.2, -0.77)
-//   gltf.scene.rotateY(Math.PI)
-//   scene.add(gltf.scene)
-// })
-// loader.load('models/suv_wheel.glb', (gltf) => {
-//   gltf.scene.position.set(-0.65, 0.2, 0.57)
-//   scene.add(gltf.scene)
-// })
-// loader.load('models/suv_wheel.glb', (gltf) => {
-//   gltf.scene.position.set(0.65, 0.2, 0.57)
-//   gltf.scene.rotateY(Math.PI)
-//   scene.add(gltf.scene)
-// })
+// Lens flare image in the position of the light (555)
+const textureLoader = new THREE.TextureLoader()
+const textureFlare0 = textureLoader.load('https://cdn.jsdelivr.net/gh/Sean-Bradley/First-Car-Shooter@main/dist/client/img/lensflare0.png')
 
-// --------------------------------------------------//
-// const loader = new GLTFLoader()
-// let suvBody: THREE.Object3D
-// await loader.loadAsync('models/suv_body.glb').then((gltf) => {
-//   suvBody = gltf.scene
-// })
-// loader.load('models/suv_wheel.glb', function (gltf) {
-//   const wheels = [gltf.scene, gltf.scene.clone(), gltf.scene.clone(), gltf.scene.clone()]
-//   wheels[0].position.set(-0.65, 0.2, -0.77)
-//   wheels[1].position.set(0.65, 0.2, -0.77)
-//   wheels[1].rotateY(Math.PI)
-//   wheels[2].position.set(-0.65, 0.2, 0.57)
-//   wheels[3].position.set(0.65, 0.2, 0.57)
-//   wheels[3].rotateY(Math.PI)
-//   suvBody.add(...wheels)
-//   scene.add(suvBody)
-// })
-// ------------------------------------------------ //
-async function loadCar() {
-  const loader = new GLTFLoader()
-  const [...model] = await Promise.all([loader.loadAsync('models/suv_body.glb'), loader.loadAsync('models/suv_wheel.glb')])
+const lensflare = new Lensflare()
+lensflare.addElement(new LensflareElement(textureFlare0, 1000, 0))
+light.add(lensflare)
+// ------------
 
-  const suvBody = model[0].scene
-  const wheels = [model[1].scene, model[1].scene.clone(), model[1].scene.clone(), model[1].scene.clone()]
+new GLTFLoader().load('models/suzanne_scene.glb', (gltf) => {
+  console.log(gltf)
 
-  wheels[0].position.set(-0.65, 0.2, -0.77)
-  wheels[1].position.set(0.65, 0.2, -0.77)
-  wheels[1].rotateY(Math.PI)
-  wheels[2].position.set(-0.65, 0.2, 0.57)
-  wheels[3].position.set(0.65, 0.2, 0.57)
-  wheels[3].rotateY(Math.PI)
-  suvBody.add(...wheels)
+  const suzanne = gltf.scene.getObjectByName('Suzanne') as THREE.Mesh
+  suzanne.castShadow = true
 
-  scene.add(suvBody)
-}
-await loadCar()
+  const plane = gltf.scene.getObjectByName('Plane') as THREE.Mesh
+  plane.receiveShadow = true
+
+  scene.add(gltf.scene)
+})
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
