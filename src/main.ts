48db61f08de5f58b599f0a947a868ec1c3c5f9d2
Luhan Beal <luhansantanna@gmail.com>
2024-06-16 16:22:59 +0100

    Orbit Controls Deep

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,18 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 import Stats from 'three/addons/libs/stats.module.js'
 
 const scene = new THREE.Scene()
 
-new RGBELoader().load('img/venice_sunset_1k.hdr', (texture) => {
-  texture.mapping = THREE.EquirectangularReflectionMapping
-  scene.environment = texture
-  scene.background = texture
-  scene.backgroundBlurriness = 0.5
-})
+const gridHelper = new THREE.GridHelper()
+gridHelper.position.y = -0.5
+scene.add(gridHelper)
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.set(0, 0, 3)
+camera.position.z = 2
 
-const renderer = new THREE.WebGLRenderer({ antialias: true })
-renderer.toneMapping = THREE.ACESFilmicToneMapping
-renderer.toneMappingExposure = 0.8
-renderer.shadowMap.enabled = true
+const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -30,80 +22,76 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
+// -- Information div added dinamicaly
+const info = document.createElement('div')
+info.style.cssText = 'position:absolute;bottom:10px;left:10px;color:white;font-family:monospace;font-size: 17px;filter: drop-shadow(1px 1px 1px #000000);'
+document.body.appendChild(info)
+
 const controls = new OrbitControls(camera, renderer.domElement)
+
+// -- CANT CHANGE JUST THE CAMERA, OrbitControls will reset the camera, we can do this instead and controls.update() on animate function
+// camera.lookAt(0.5, 0.5, 0.5)
+controls.target.set(.5, .5, .5)
+// controls.update()
+
+// -- EVENT LISTENERS to orbitControll -- 
+controls.addEventListener('change', () => {
+    // -- info added to div dinamically (dont need to be updated on animate just onchange event)
+    info.innerText =
+    'Polar Angle : ' +
+    ((controls.getPolarAngle() / -Math.PI) * 180 + 90).toFixed(2) +
+    '°\nAzimuth Angle : ' +
+    ((controls.getAzimuthalAngle() / Math.PI) * 180).toFixed(2) +
+    '°'
+})
+controls.addEventListener('start', () => console.log("Controls Start Event"))
+controls.addEventListener('end', () => console.log("Controls End Event"))
+//-- autorotate --
+// controls.autoRotate = true
+// controls.autoRotateSpeed = 10
+// -- smooth change when click up
 controls.enableDamping = true
+// -- Very slow change when click up
+controls.dampingFactor = .01
+// -- controls with the arrows on keyboard
+// controls.listenToKeyEvents(window)
+// -- customize keys --
+controls.keys = {
+    LEFT: 'KeyA', // default 'ArrowLeft'
+    UP: 'KeyW', // default 'ArrowUp'
+    RIGHT: 'KeyD', // default 'ArrowRight'
+    BOTTOM: 'KeyS' // default 'ArrowDown'
+}
+controls.mouseButtons = {
+    LEFT: THREE.MOUSE.ROTATE,
+    MIDDLE: THREE.MOUSE.DOLLY,
+    RIGHT: THREE.MOUSE.PAN
+}
+controls.touches = {
+    ONE: THREE.TOUCH.ROTATE,
+    TWO: THREE.TOUCH.DOLLY_PAN
+}
+// -- change grab to up and down
+// controls.screenSpacePanning = true
+// -- limit directional(azimuth) angle to move
+controls.minAzimuthAngle = 0
+controls.maxAzimuthAngle = Math.PI / 2 // 90
+controls.minPolarAngle = 0
+controls.maxPolarAngle = Math.PI // 180
+// -- min/max distance
+controls.maxDistance = 4
+controls.minDistance = 1.5
+// -- disable controls
+// controls.enabled = false
+// controls.enablePan = false
+// controls.enableRotate = false
+// controls.enableZoom = false
 
-const raycaster = new THREE.Raycaster()
-const pickables: THREE.Mesh[] = []
-const mouse = new THREE.Vector2()
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
-const arrowHelper = new THREE.ArrowHelper()
-arrowHelper.setLength(0.5)
-scene.add(arrowHelper)
-
-renderer.domElement.addEventListener('mousemove', (e) => {
-  mouse.set((e.clientX / renderer.domElement.clientWidth) * 2 - 1, -(e.clientY / renderer.domElement.clientHeight) * 2 + 1)
-
-  raycaster.setFromCamera(mouse, camera)
-
-  const intersects = raycaster.intersectObjects(pickables, false)
-
-  if (intersects.length) {
-    //     //console.log(intersects.length)
-    //     //console.log(intersects[0].point)
-    //     //console.log(intersects[0].object.name + ' ' + intersects[0].distance)
-    //     //console.log((intersects[0].face as THREE.Face).normal)
-
-    // SET THE NORMAL ARROW 
-    const n = new THREE.Vector3()
-    n.copy((intersects[0].face as THREE.Face).normal)
-    n.transformDirection(intersects[0].object.matrixWorld)
-
-    arrowHelper.setDirection(n)
-    arrowHelper.position.copy(intersects[0].point)
-  }
-})
-
-renderer.domElement.addEventListener('dblclick', (e) => {
-  mouse.set((e.clientX / renderer.domElement.clientWidth) * 2 - 1, -(e.clientY / renderer.domElement.clientHeight) * 2 + 1)
-
-  raycaster.setFromCamera(mouse, camera)
-
-  const intersects = raycaster.intersectObjects(pickables, false)
-
-  if (intersects.length) {
-    const n = new THREE.Vector3()
-    n.copy((intersects[0].face as THREE.Face).normal)
-    n.transformDirection(intersects[0].object.matrixWorld)
-
-    const cube = new THREE.Mesh(new THREE.BoxGeometry(0.2, 0.2, 0.2), new THREE.MeshStandardMaterial())
-    cube.lookAt(n)
-    cube.position.copy(intersects[0].point)
-    cube.position.addScaledVector(n, 0.1)
-    cube.castShadow = true
-
-    scene.add(cube)
-    pickables.push(cube)
-  }
-})
-
-new GLTFLoader().load('models/suzanne_scene.glb', (gltf) => {
-  const suzanne = gltf.scene.getObjectByName('Suzanne') as THREE.Mesh
-  suzanne.castShadow = true
-  // @ts-ignore
-  suzanne.material.map.colorSpace = THREE.LinearSRGBColorSpace
-  pickables.push(suzanne)
-
-  const plane = gltf.scene.getObjectByName('Plane') as THREE.Mesh
-  plane.receiveShadow = true
-  pickables.push(plane)
-
-  const spotLight = gltf.scene.getObjectByName('Spot') as THREE.SpotLight
-  spotLight.intensity /= 500
-  spotLight.castShadow = true
-
-  scene.add(gltf.scene)
-})
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
