25fc123e8541d771ff7cc4a7d67d67b618f3d505
Luhan Beal <luhansantanna@gmail.com>
2024-06-16 15:28:14 +0100

    Raycaster

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,22 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 import Stats from 'three/addons/libs/stats.module.js'
-import { Lensflare, LensflareElement } from 'three/addons/objects/Lensflare.js'
 
 const scene = new THREE.Scene()
-
-const light = new THREE.SpotLight(undefined, Math.PI * 1000)
-light.position.set(5, 5, 5)
-light.angle = Math.PI / 16
-light.castShadow = true
-scene.add(light)
 
 new RGBELoader().load('img/venice_sunset_1k.hdr', (texture) => {
   texture.mapping = THREE.EquirectangularReflectionMapping
   scene.environment = texture
   scene.background = texture
+  scene.backgroundBlurriness = 0.5
 })
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.set(1.5, 0.75, 2)
+camera.position.set(0, 0, 3)
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.toneMapping = THREE.ACESFilmicToneMapping
-renderer.toneMappingExposure = 0.1
+renderer.toneMappingExposure = 0.8
 renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
@@ -39,23 +33,74 @@ window.addEventListener('resize', () => {
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
+const raycaster = new THREE.Raycaster()
+const pickables: THREE.Mesh[] = []
+const mouse = new THREE.Vector2()
+
+const arrowHelper = new THREE.ArrowHelper()
+arrowHelper.setLength(0.5)
+scene.add(arrowHelper)
+
+renderer.domElement.addEventListener('mousemove', (e) => {
+  mouse.set((e.clientX / renderer.domElement.clientWidth) * 2 - 1, -(e.clientY / renderer.domElement.clientHeight) * 2 + 1)
+
+  raycaster.setFromCamera(mouse, camera)
+
+  const intersects = raycaster.intersectObjects(pickables, false)
+
+  if (intersects.length) {
+    //     //console.log(intersects.length)
+    //     //console.log(intersects[0].point)
+    //     //console.log(intersects[0].object.name + ' ' + intersects[0].distance)
+    //     //console.log((intersects[0].face as THREE.Face).normal)
+
+    // SET THE NORMAL ARROW 
+    const n = new THREE.Vector3()
+    n.copy((intersects[0].face as THREE.Face).normal)
+    n.transformDirection(intersects[0].object.matrixWorld)
+
+    arrowHelper.setDirection(n)
+    arrowHelper.position.copy(intersects[0].point)
+  }
+})
+
+renderer.domElement.addEventListener('dblclick', (e) => {
+  mouse.set((e.clientX / renderer.domElement.clientWidth) * 2 - 1, -(e.clientY / renderer.domElement.clientHeight) * 2 + 1)
+
+  raycaster.setFromCamera(mouse, camera)
+
+  const intersects = raycaster.intersectObjects(pickables, false)
+
+  if (intersects.length) {
+    const n = new THREE.Vector3()
+    n.copy((intersects[0].face as THREE.Face).normal)
+    n.transformDirection(intersects[0].object.matrixWorld)
+
+    const cube = new THREE.Mesh(new THREE.BoxGeometry(0.2, 0.2, 0.2), new THREE.MeshStandardMaterial())
+    cube.lookAt(n)
+    cube.position.copy(intersects[0].point)
+    cube.position.addScaledVector(n, 0.1)
+    cube.castShadow = true
+
+    scene.add(cube)
+    pickables.push(cube)
+  }
+})
+
 new GLTFLoader().load('models/suzanne_scene.glb', (gltf) => {
   const suzanne = gltf.scene.getObjectByName('Suzanne') as THREE.Mesh
   suzanne.castShadow = true
+  // @ts-ignore
+  suzanne.material.map.colorSpace = THREE.LinearSRGBColorSpace
+  pickables.push(suzanne)
 
   const plane = gltf.scene.getObjectByName('Plane') as THREE.Mesh
   plane.receiveShadow = true
+  pickables.push(plane)
 
-  const spotlight = gltf.scene.getObjectByName('Spot') as THREE.SpotLight
-  spotlight.intensity /= 500
-  spotlight.castShadow = true
-
-  const textureLoader = new THREE.TextureLoader()
-  const textureFlare0 = textureLoader.load('https://cdn.jsdelivr.net/gh/Sean-Bradley/First-Car-Shooter@main/dist/client/img/lensflare0.png')
-
-  const lensflare = new Lensflare()
-  lensflare.addElement(new LensflareElement(textureFlare0, 1000, 0))
-  spotlight.add(lensflare)
+  const spotLight = gltf.scene.getObjectByName('Spot') as THREE.SpotLight
+  spotLight.intensity /= 500
+  spotLight.castShadow = true
 
   scene.add(gltf.scene)
 })
